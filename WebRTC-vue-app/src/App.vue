ade6ca3017225392a465f83934d990f2415e6335
mtuan <tuanxsokoh@gmail.com>
2022-10-12 20:42:18 +0700

    first commit

---
+++ WebRTC-vue-app/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import PageWrapper from './components/PageWrapper.vue';
+import HomePage from './components/HomePage.vue';
+</script>
+
+<template>
+  <PageWrapper><HomePage /></PageWrapper>
+</template>
+
+<style scoped>
+.m-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+}
+</style>
---
+++ WebRTC-vue-app/src/components/HomePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mockup-window border bg-base-300 w-4/5 h-4/5">
+    <div class="flex justify-center px-4 py-16 bg-base-200 h-full bg-gray-700">
+      <div class="flex flex-col gap-8 mt-10">
+        <div class="form-control w-full max-w-xs">
+          <label class="label">
+            <span class="label-text text-white">Nhập tên của bạn</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered w-full max-w-xs"
+          />
+        </div>
+        <button class="btn btn-outline btn-warning">Tạo phòng mới</button>
+        <button class="btn btn-outline btn-warning">Tham gia phòng</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
